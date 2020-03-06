b1d2f7bfe4d074aa40512c74ae79c4c062ce403b
ckeyang <463650967@qq.com>
2020-03-06 17:58:46 +0800

    提交一下

--- components/NTableColumn.vue
+++ components/NTableColumn.vue
@@ -2,22 +2,17 @@
     table 列表
 <!-->
 <script>
-    export default {
-        name:'NTableColumn',
-        install (Vue, theme) {
-            selfInstall(Vue, theme, this);
-        },
-        props:{
-        },
-        computed:{
-        },
-        methods:{
-        },
-        watch:{},
-        render:function(createElement){
-                return createElement(
-                    'span',this.$slots.default
-                )
-        }
-    }
+export default {
+  name: "NTableColumn",
+  install(Vue, theme) {
+    selfInstall(Vue, theme, this);
+  },
+  props: {},
+  computed: {},
+  methods: {},
+  watch: {},
+  render: function(createElement) {
+    return createElement("span", this.$slots.default);
+  }
+};
 </script>
